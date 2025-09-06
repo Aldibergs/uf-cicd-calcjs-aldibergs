615fdb580af9b7906631d325a04a8272bca07691
Aldibergs <aldibergs@gmail.com>
2025-09-06 14:43:13 -0300

    Quarto Commit

--- src/calculadora.js
+++ src/calculadora.js
@@ -1,20 +1,21 @@
 // Função de adição
-    function add(a, b) {
-        return a + b;
-    }   
+function add(a, b) {
+    return a + b;
+}
 
-    // Exportar as funções
-    module.exports = {
-        add
-    };  
+// Função de subtração
+function subtract(a, b) {
+    return a - b;
+}
 
-    // Função de subtração
-    function subtract(a, b) {
-        return a - b;
-    }
+// Função de multiplicação
+function multiply(a, b) {
+    return a * b;
+}
 
-    // Exportar as funções
-    module.exports = {
-        add,
-        subtract
-    };
+// Exportar as funções
+module.exports = {
+    add,
+    subtract,
+    multiply
+};
